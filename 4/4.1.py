98866a0aa9db58a03c5e042b72454f3dcaf3c0a9
Jimmy <jimmy_chen_7@hotmail.com>
2016-12-10 13:33:15 +1100

    finished day 4 part 2

--- 4/4.1.py
+++ 4/4.1.py
@@ -4,7 +4,7 @@ By: Jimmy Chen (https://github.com/jimmychen7)
 Date: 07/12/16
 """
 
-import re, operator
+import re
 
 f = open('4.txt', 'r')
 
@@ -12,8 +12,7 @@ sum_sector_id = 0
 for line in f:
     line.rstrip()
     sections = re.split('-', line)
-    #print(sections)
-    
+
     letter_count = {}
     for section in sections[:-1]:
         for letter in section:
@@ -24,35 +23,47 @@ for line in f:
     sorted_keys = []
     for key in sorted(letter_count, key=letter_count.get, reverse=True):
         sorted_keys.append(key)
-    
-    
+
     sector_id = re.search('\d+', sections[-1]).group(0)
     checksum = re.search('[^\d\[\]]+', sections[-1]).group(0)
-    
-    #rearrange sorted_keys so that keys that give equal value is sorted alphabetically
+
+    # rearrange sorted_keys so that keys that give equal
+    # value are sorted alphabetically
     for index, key in enumerate(sorted_keys):
-        if index < len(sorted_keys)-1:
-            if letter_count[key] == letter_count[sorted_keys[index+1]]:
-                if key > sorted_keys[index+1]:
-                    sorted_keys[index] = sorted_keys[index+1]
-                    sorted_keys[index+1] = key
-    
-    print("".join(sorted_keys[:5])) 
-            
+        if index < len(sorted_keys) - 1:
+            if letter_count[key] == letter_count[sorted_keys[index + 1]]:
+                if key > sorted_keys[index + 1]:
+                    sorted_keys[index] = sorted_keys[index + 1]
+                    sorted_keys[index + 1] = key
+
+    # print("".join(sorted_keys[:5]))
+
     for index, letter in enumerate(checksum):
-        if sorted_keys[index] != letter:            
+        if sorted_keys[index] != letter:
             break
 
     if index < 4:
-        print("%s %s %s\n" % (checksum, sector_id, sum_sector_id))
         continue
     else:
-        sum_sector_id += int(sector_id)
-        print("%s %s %s ############ real #############\n" % (checksum, sector_id, sum_sector_id))
+        # real room
+        decrypted_string = ""
 
-print(sum_sector_id)
-        
-    
-            
+        for section in sections[:-1]:
+            for letter in section:
+                decrypted_letter = (chr((
+                                    ord(letter.lower()) +
+                                    int(sector_id) % 26)))
 
+                if decrypted_letter > 'z':
+                    decrypted_letter = (chr(
+                                        ord('a') +
+                                        ord(decrypted_letter) -
+                                        ord('z') - 1))
+
+                decrypted_string += decrypted_letter
+            decrypted_string += " "
+
+        if re.search("object", decrypted_string):
+            print("-".join(sections))
+            print(decrypted_string)
 
