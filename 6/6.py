485ea86d1e2b00c96fc76927d851f631586452e4
Jimmy <jimmy_chen_7@hotmail.com>
2016-12-14 01:58:29 +1100

    finished day 6 part 1

--- 6/6.py
+++ 6/6.py
@@ -1,7 +1,29 @@
+#!/usr/bin/env python
+
 """
 http://adventofcode.com/2016/day/
-By: Jimmy Chen
+By: Jimmy Chen (https://github.com/jimmychen7)
 Date:
 """
+import operator
+import sys
 
+f = open('6.txt', 'r')
+lines = f.readlines()
+count = []
 
+for letter in lines[0].rstrip():
+    count.append({})
+
+for line in lines[1:]:
+    line = line.rstrip()
+    for col, letter in enumerate(line):
+        if letter in count[col]:
+            count[col][letter] += 1
+        else:
+            count[col][letter] = 1
+
+for letter_count in count:
+    sorted_count = sorted(letter_count.items(), key=operator.itemgetter(1))
+    sorted_count.reverse()
+    sys.stdout.write(sorted_count[0][0])
